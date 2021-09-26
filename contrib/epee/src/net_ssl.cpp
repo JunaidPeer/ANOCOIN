3c09b8a6849402c2cb2aa65677bb6137d2dafb84
JunaidPeer <87606844+JunaidPeer@users.noreply.github.com>
2021-09-26 13:40:11 +0530

    ANOCOIN

--- contrib/epee/src/net_ssl.cpp
+++ contrib/epee/src/net_ssl.cpp
@@ -38,8 +38,8 @@
 #include "net/net_helper.h"
 #include "net/net_ssl.h"
 
-#undef SCALA_DEFAULT_LOG_CATEGORY
-#define SCALA_DEFAULT_LOG_CATEGORY "net.ssl"
+#undef ANOCOIN_DEFAULT_LOG_CATEGORY
+#define ANOCOIN_DEFAULT_LOG_CATEGORY "net.ssl"
 
 // openssl genrsa -out /tmp/KEY 4096
 // openssl req -new -key /tmp/KEY -out /tmp/REQ
