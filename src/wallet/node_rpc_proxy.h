f8d7585a87c1f5a56a6232b22b408eccb49bd924
JunaidPeer <87606844+JunaidPeer@users.noreply.github.com>
2021-09-28 14:09:59 +0530

    ANOCOIN

--- src/wallet/node_rpc_proxy.h
+++ src/wallet/node_rpc_proxy.h
@@ -1,6 +1,5 @@
 // Copyright (c) 2017-2020, The Monero Project
-// 
-// All rights reserved.
+// // All rights reserved.
 // 
 // Redistribution and use in source and binary forms, with or without modification, are
 // permitted provided that the following conditions are met:
